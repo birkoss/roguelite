2a2d1043dfe1f1aec92573449ce34c178815e2b4
Mathieu Robichaud <admin@birkoss.com>
2025-01-29 12:54:51 -0500

    Refactor Panel

--- src/panel.js
+++ src/panel.js
@@ -1,14 +1,33 @@
 import { PANEL_ASSET_KEYS, UNIT_ASSET_KEYS } from "./keys/asset.js";
 
 export class Panel {
+
+    static #CONFIG = {
+        width: 0,
+        height: 168,
+        padding: 5,
+        colors: {
+            background: 0x333333,
+            frameBorder: 0x000000,
+            frameEnemy: 0x9e2b18,
+            framePlayer: 0x139c13,
+            line: 0x646464,
+            shadow: 0x000000,
+            health: 0xd40200,
+            mana: 0x5487ff,
+            coin: 0x8c7533,
+            
+            enemy: 0x4db02f,
+            player: 0x07bafc,
+        }
+    };
+
     /** @type {Phaser.Scene} */
     #scene;
     #container;
+    #config;
 
-    #textHealth;
-    #textCoin;
-    #textMana;
-    #textEnemy;
+    #units;
 
     /**
      * @param {Phaser.Scene} scene - The scene this panel belongs to
@@ -17,91 +36,113 @@ export class Panel {
      */
     constructor(scene, x, y) {
         this.#scene = scene;
-
         this.#container = scene.add.container(x, y);
 
-        let background = scene.add.rectangle(0, 0, this.#scene.game.canvas.width, 168, 0x333333).setOrigin(0, 0);
-        this.#container.add(background);
+        this.#config = {
+            ...Panel.#CONFIG,
+            width: this.#scene.game.canvas.width,
+        };
 
-        let line = scene.add.rectangle(0, background.y + background.height, this.#scene.game.canvas.width, 2, 0x646464).setOrigin(0, 0);
-        this.#container.add(line);
+        this.#container.add([
+            this.#createBackground(0, this.#config.height, this.#config.colors.background),
+            this.#createBackground(174, 2, this.#config.colors.line),
+            this.#createBackground(176, 16, this.#config.colors.shadow, 0.2),
 
-        let shadow = scene.add.rectangle(0, line.y + line.height, this.#scene.game.canvas.width, 16, 0x000000, 0.2).setOrigin(0, 0);
-        this.#container.add(shadow);
+            scene.add.sprite(0, 24, PANEL_ASSET_KEYS.BACKGROUND).setOrigin(0).setTint(0x595d8c),
+        ]);
 
-        let bg = scene.add.sprite(0, 24, PANEL_ASSET_KEYS.BACKGROUND).setOrigin(0).setTint(0x595d8c);
+        this.#units = [];
+
+        this.#createStatPanel("Your Knight", 0);
+        this.#createStatPanel("Goblin", 195, true);
+    }
+
+    #createBackground(y, height, color, alpha = 1) {
+        return this.#scene.add.rectangle(
+            0, y, 
+            this.#config.width, 
+            height, 
+            color
+        ).setOrigin(0, 0).setAlpha(alpha);
+    }
+
+    #createIcon(x, y, frame, tint, scale = 2) {
+        return this.#scene.add.sprite(x, y, PANEL_ASSET_KEYS.ICON, frame)
+            .setOrigin(0)
+            .setTint(tint)
+            .setScale(scale);
+    }
+
+    #createText(x, y, text, size = 20, alpha = 1) {
+        return this.#scene.add.bitmapText(
+            x, y, 
+            PANEL_ASSET_KEYS.FONT, 
+            text, 
+            size
+        ).setTint(this.#config.colors.text)
+         .setOrigin(0)
+         .setAlpha(alpha);
+    }
+
+    #createStatPanel(unitName, unitFrame, isRight = false) {
+        const origin = (isRight ? 1 : 0);
+
+        // Create the frame for the name
+        const frameNameY = (isRight ? 0 : 150);
+        const frameColor = (isRight ? this.#config.colors.frameEnemy : this.#config.colors.framePlayer);
+        this.#container.add(this.#createBackground(frameNameY, 24, this.#config.colors.frameBorder));
+        this.#container.add(this.#createBackground(frameNameY+4, 16, frameColor));
+
+        // Create the name
+        const name = this.#scene.add.bitmapText(0, frameNameY + 1, PANEL_ASSET_KEYS.FONT, unitName, 20).setTint(0xffffff).setOrigin(origin, 0);
+        name.x = (isRight ? this.#scene.game.canvas.width - this.#config.padding : this.#config.padding);
+        this.#container.add(name);
+
+        const unitY = 124;
+        const unitColor = (isRight ? this.#config.colors.enemy : this.#config.colors.player);
+        const unitX = this.#config.width / 2 + (isRight ? 50 : -50);
+
+        // Create the unit shadow
+        const unitShadow = this.#scene.add.sprite(unitX - 2, unitY, UNIT_ASSET_KEYS.BACKGROUND, unitFrame).setOrigin(0.5).setTint(this.#config.colors.shadow);
+        if (!isRight) {
+            unitShadow.scaleX = -1;
+        }
+        this.#container.add(unitShadow);
+
+        // Create the unit
+        const unit = this.#scene.add.sprite(unitX, unitY, UNIT_ASSET_KEYS.BACKGROUND, unitFrame).setOrigin(0.5).setTint(unitColor);
+        if (!isRight) {
+            unit.scaleX = -1;
+        }
+        this.#container.add(unit);
+        this.#units.push(unit);
+
+        const xPos = isRight ? this.#config.width : this.#config.padding;
+
+        const bg = this.#createBackground(24, 120, this.#config.colors.shadow, 0.3);
+        bg.width = 40;
+        bg.setOrigin(origin, 0);
+        bg.x = xPos + (isRight ? 0 : -this.#config.padding);
         this.#container.add(bg);
 
-        let playerShadow = scene.add.sprite(165, bg.y + bg.height - 20, UNIT_ASSET_KEYS.BACKGROUND, 0).setOrigin(0.5).setTint(0x000000);
-        playerShadow.scaleX = -1;
-        this.#container.add(playerShadow);
+        const stats = [
+            { frame: 0, color: this.#config.colors.health, text: "10/10" },
+            { frame: 1, color: this.#config.colors.mana, text: "10/99" },
+        ];
+        if (!isRight) {
+            stats.push({ frame: 2, color: this.#config.colors.coin, text: "230" });
+        }
 
-        let player = scene.add.sprite(163, bg.y + bg.height - 20, UNIT_ASSET_KEYS.BACKGROUND, 0).setOrigin(0.5).setTint(0x08bafc);
-        player.scaleX = -1;
-        this.#container.add(player);
-
-        playerShadow = scene.add.sprite(252, bg.y + bg.height - 20, UNIT_ASSET_KEYS.BACKGROUND, 195).setOrigin(0.5).setTint(0x000000);
-        this.#container.add(playerShadow);
-
-        player = scene.add.sprite(250, bg.y + bg.height - 20, UNIT_ASSET_KEYS.BACKGROUND, 195).setOrigin(0.5).setTint(0x4db02f);
-        this.#container.add(player);
-        
-        background = scene.add.rectangle(0, 0, this.#scene.game.canvas.width, 24, 0x000000).setOrigin(0, 0);
-        this.#container.add(background);
-
-        background = scene.add.rectangle(0, 4, this.#scene.game.canvas.width, 16, 0x9e2b18).setOrigin(0, 0);
-        this.#container.add(background);
-
-        this.#textEnemy = scene.add.bitmapText(0, 1, PANEL_ASSET_KEYS.FONT, "Goblin", 20).setTint(0xffffff).setOrigin(1, 0);
-        this.#textEnemy.x = this.#scene.game.canvas.width - 5;
-        this.#container.add(this.#textEnemy);
-
-        background = scene.add.rectangle(0, bg.y + bg.height + 10, this.#scene.game.canvas.width, 24, 0x000000).setOrigin(0, 0);
-        this.#container.add(background);
-
-        background = scene.add.rectangle(0, bg.y + bg.height + 14, this.#scene.game.canvas.width, 16, 0x139c13).setOrigin(0, 0);
-        this.#container.add(background);
-
-        this.#textEnemy = scene.add.bitmapText(5, bg.y + bg.height + 11, PANEL_ASSET_KEYS.FONT, "Your Knight", 20).setTint(0xffffff).setOrigin(0, 0);
-        this.#container.add(this.#textEnemy);
-
-        // player
-        background = scene.add.rectangle(0, 24, 40, 120, 0x000000).setOrigin(0, 0).setAlpha(0.3);
-        this.#container.add(background);
-
-        player = scene.add.sprite(5, 26, PANEL_ASSET_KEYS.ICON, 0).setOrigin(0).setTint(0xd40200).setScale(2);
-        this.#container.add(player);
-        this.#textEnemy = scene.add.bitmapText(45, 29, PANEL_ASSET_KEYS.FONT, "10/10", 30).setTint(0xffffff).setOrigin(0).setAlpha(0.8);;
-        this.#container.add(this.#textEnemy);
-
-        player = scene.add.sprite(5, 64, PANEL_ASSET_KEYS.ICON, 1).setOrigin(0).setTint(0x5487ff).setScale(2);
-        this.#container.add(player);
-        this.#textEnemy = scene.add.bitmapText(45, 68, PANEL_ASSET_KEYS.FONT, "10/99", 30).setTint(0xffffff).setOrigin(0).setAlpha(0.8);;
-        this.#container.add(this.#textEnemy);
-        
-        player = scene.add.sprite(5, 106, PANEL_ASSET_KEYS.ICON, 2).setOrigin(0).setTint(0x8c7533).setScale(2);
-        this.#container.add(player);
-
-        this.#textEnemy = scene.add.bitmapText(45, 108, PANEL_ASSET_KEYS.FONT, "230", 30).setTint(0xffffff).setOrigin(0).setAlpha(0.8);;
-        this.#container.add(this.#textEnemy);
-
-        background = scene.add.rectangle(0, 24, 40, 120, 0x000000).setOrigin(1, 0).setAlpha(0.3);
-        background.x = this.#scene.game.canvas.width;
-        this.#container.add(background);
-
-        player = scene.add.sprite(5, 26, PANEL_ASSET_KEYS.ICON, 0).setOrigin(1, 0).setTint(0xd40200).setScale(2);
-        player.x = this.#scene.game.canvas.width - 4;
-        this.#container.add(player);
-        this.#textEnemy = scene.add.bitmapText(45, 29, PANEL_ASSET_KEYS.FONT, "10/10", 30).setTint(0xffffff).setOrigin(1, 0).setAlpha(0.8);;
-        this.#textEnemy.x = this.#scene.game.canvas.width - 45;
-        this.#container.add(this.#textEnemy);
-
-        player = scene.add.sprite(5, 64, PANEL_ASSET_KEYS.ICON, 1).setOrigin(1, 0).setTint(0x5487ff).setScale(2);
-        player.x = this.#scene.game.canvas.width - 4;
-        this.#container.add(player);
-        this.#textEnemy = scene.add.bitmapText(45, 68, PANEL_ASSET_KEYS.FONT, "10/99", 30).setTint(0xffffff).setOrigin(1, 0).setAlpha(0.8);
-        this.#textEnemy.x = this.#scene.game.canvas.width - 45;
-        this.#container.add(this.#textEnemy);
-
+        stats.forEach((singleStat, index) => {
+            const y = 26 + (index * 42);
+            const icon = this.#createIcon(xPos - (isRight ? 4 : 0), y, singleStat.frame, singleStat.color);
+            icon.setOrigin(origin, 0);
+            
+            const label = this.#createText(xPos - (isRight ? 45 : -45), y + 3, singleStat.text, 30, 0.8);
+            label.setOrigin(origin, 0);
+            
+            this.#container.add([icon, label]);
+        });
     }
+
 }
